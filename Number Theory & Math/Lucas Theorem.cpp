ad7aa929def2c1103c05b44fa879e480c2f5085e
Nondon Chandra Roy <nondonroy.me@gmail.com>
2026-09-01 00:49:20 +0600

    Create Lucas Theorem.cpp

---
+++ Number Theory & Math/Lucas Theorem.cpp
@@ -0,0 +1,15 @@
+// nCr mod Prime
+//M is Prime
+#define ll long long int
+ll lucas(ll n, ll r) {
+    ll ans = 1;
+    while(n || r) {
+        ll ni = n % M;
+        ll ri = r % M;
+        if(ri > ni) return 0;
+        n /= M;
+        r /= M;
+        ans = (ans * nCr(ni, ri)) % M;
+    }
+    return ans;
+}
