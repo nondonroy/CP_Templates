424854dc53f1cd2ca8945a6b4d396e869256b7d6
Nondon Chandra Roy <nondonroy.me@gmail.com>
2026-09-18 02:12:14 +0600

    Create Bellman Ford.cpp

---
+++ Graph/Bellman Ford.cpp
@@ -0,0 +1,24 @@
+vector<ll> bellmanFord(ll source, vector<vector<ll>> &edges) {
+    vector<ll> dist(n + 1, INF);
+
+    dist[source] = 0;
+
+    for(ll i = 0; i < n - 1; i++) {
+        for(auto it : edges) {
+            ll u = it[0];
+            ll v = it[1];
+            ll wt = it[2];
+
+            if(dist[u] == INF) continue;
+
+            if(dist[u] + wt < dist[v]) {
+                dist[v] = dist[u] + wt;
+            }
+        }
+    }
+
+    return dist;
+}
+// O(VE)
+/* in n'th iteration if the distance reduced then
+there exist negative cycle */
