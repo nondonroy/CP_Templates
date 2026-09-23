08912973394e70530f7a5a99ecb0d49fd8469836
Nondon Chandra Roy <nondonroy.me@gmail.com>
2026-09-23 14:06:07 +0600

    Modify Bellman-Ford algorithm with path finding
    Updated the INF constant value and added path reconstruction functionality.

--- Graph/Bellman Ford.cpp
+++ Graph/Bellman Ford.cpp
@@ -1,9 +1,10 @@
 #define ll long long int
-const ll INF = 1e15;
+const ll INF = 1e9;
 
+ll n;
+vector<ll> dist(n + 1, INF);
+vector<ll> par(n + 1, -1);
 vector<ll> bellmanFord(ll source, vector<vector<ll>> &edges) {
-    vector<ll> dist(n + 1, INF);
-
     dist[source] = 0;
 
     for(ll i = 0; i < n - 1; i++) {
@@ -16,11 +17,26 @@ vector<ll> bellmanFord(ll source, vector<vector<ll>> &edges) {
 
             if(dist[u] + wt < dist[v]) {
                 dist[v] = dist[u] + wt;
+                par[v] = u;
             }
         }
     }
-
     return dist;
+}
+vector<ll> findPath(ll source, ll des) {
+    vector<ll> path;
+    
+    if(dist[des] == INF) // no path
+        return path;
+    
+    path.push_back(des);
+    ll cur = des;
+    while(cur != source) {
+        path.push_back(par[cur]);
+        cur = par[cur];
+    }
+    reverse(path.begin(), path.end());
+    return path;
 }
 //SPSP - worked in negative edges
 // O(VE)
